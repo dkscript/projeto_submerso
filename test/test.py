904c3324f9c11e9a6406fee3c75d0ddc4e37a521
Flavio nunes dos santos <dkscript@gmail.com>
2022-12-13 16:40:04 -0300

    .

--- test/test.py
+++ test/test.py
@@ -1,4 +1,16 @@
-from src.lib.stings import remove_vogal
+from ..src.lib.stings import remove_vogal
 
 def test_remove_vogal():
-    assert remove_vogal('teste') == 'tst'
+   assert remove_vogal('teste') == 'tst'
+
+def test_remove_vogal_aeiou():
+   assert remove_vogal('aeiou') == ''
+
+def test_remove_vogal_123():
+    assert remove_vogal(123) == ''
+
+def test_remove_vogal_1b3c():
+   assert remove_vogal('1b3c') == '1b3c'
+
+def test_remove_vogal_123():
+   assert remove_vogal('qwrtd') == 'qwrtd'
